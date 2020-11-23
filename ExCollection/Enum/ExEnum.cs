1b644b2ce224dac84addc3f0f195b285b259fe14
xhq <xhq@allfully.com>
2020-11-23 17:46:17 +0800

    添加枚举值获取Description描述内容代码 添加使用GZip+System.Text.Json压缩与解压的扩展方法

---
+++ ExCollection/Enum/ExEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+
+namespace ExCollection
+{
+    public static class ExEnum
+    {
+        /// <summary>
+        /// 获取枚举描述 搭配<Description>特性使用
+        /// </summary>
+        /// <param name="enumValue">枚举值</param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum enumValue)
+        {
+            var type = enumValue.GetType();
+            var field = type.GetField(enumValue.ToString());
+            var desc = field.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
+            if (desc != null)
+            {
+                return desc.Description;
+            }
+            return string.Empty;
+        }
+    }
+}
