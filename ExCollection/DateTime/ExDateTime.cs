05f498f13651c3db1fca3abb5d058156b5748d0e
xhq <xhq@allfully.com>
2024-03-15 11:58:46 +0800

    更新部分依赖包 新增两个时间判断是否在一天，一周，一月，一季度，半年，一年内（均为自然周月季度年）

--- ExCollection/DateTime/ExDateTime.cs
+++ ExCollection/DateTime/ExDateTime.cs
@@ -21,5 +21,99 @@ namespace ExCollection
             stopTime = startTime.AddMonths(3).AddMilliseconds(-1);
             return (startTime.Month + 2) / 3;
         }
+        /// <summary>
+        /// 比较两个时间点是否属于同一自然周
+        /// </summary>
+        /// <param name="Time"></param>
+        /// <param name="Time2"></param>
+        /// <returns></returns>
+        public static bool IsInWeek(this DateTime Time, DateTime Time2)
+        {
+            bool flag = false;
+            var largerTime = Time > Time2 ? Time : Time2;
+            var lessTime = Time < Time2 ? Time : Time2;
+            var ts = largerTime - lessTime;
+            var totalDays = ts.TotalDays;
+            var largerWeek = (int)largerTime.DayOfWeek;
+            if (largerWeek == 0)
+            {
+                largerWeek = 7;
+            }
+            if (totalDays >= 7 || totalDays >= largerWeek)
+            {
+                flag = false;
+            }
+            else
+            {
+                flag = true;
+            }
+            return flag;
+        }
+        /// <summary>
+        /// 比较两个时间点是否属于同一自然月
+        /// </summary>
+        /// <param name="Time"></param>
+        /// <param name="Time2"></param>
+        /// <returns></returns>
+        public static bool IsInMonth(this DateTime Time, DateTime Time2)
+        {
+            bool flag = true;
+            if (Time.Year == Time2.Year && Time.Month == Time2.Month)
+            {
+                flag = true;
+            }
+            else
+            {
+                flag = false;
+            }
+            return flag;
+        }
+        /// <summary>
+        /// 比较两个时间点是否属于同一季度
+        /// </summary>
+        /// <param name="Time"></param>
+        /// <param name="Time2"></param>
+        /// <returns></returns>
+        public static bool IsInQuarterTime(this DateTime Time, DateTime Time2)
+        {
+            int quarter1 = Time.GetQuarterTime(out _, out _);
+            int quarter2 = Time.GetQuarterTime(out _, out _);
+            return quarter1 == quarter2;
+        }
+        /// <summary>
+        /// 比较两个时间点是否属于同一半年（自然年）
+        /// </summary>
+        /// <param name="Time"></param>
+        /// <param name="Time2"></param>
+        /// <returns></returns>
+        public static bool IsInHalfYear(this DateTime Time, DateTime Time2)
+        {
+            bool flag = false;
+            if (Time.Year == Time2.Year)
+            {
+                var t1 = Time.Month / 7 > 0;
+                var t2 = Time2.Month / 7 > 0;
+                if (t1 == t2)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+        /// <summary>
+        /// 比较两个时间点是否属于同一年（自然年）
+        /// </summary>
+        /// <param name="Time"></param>
+        /// <param name="Time2"></param>
+        /// <returns></returns>
+        public static bool IsInYear(this DateTime Time, DateTime Time2)
+        {
+            bool flag = false;
+            if (Time.Year == Time2.Year)
+            {
+                flag = true;
+            }
+            return flag;
+        }
     }
 }
